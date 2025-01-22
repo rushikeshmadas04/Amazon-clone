d7ce4db8a706fea7aea9031bed07167b6f60dc2d
Rushikesh Madas <madasrushi900@gmail.com>
2025-01-22 12:17:21 +0530

    Learn promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -88,7 +88,7 @@ export function loadProducts(fun){
       }
       return new Product(productDetails);
     });
-
+    console.log('loadProduct');
     fun();
   });
   xhr.open('GET','https://supersimplebackend.dev/products');
